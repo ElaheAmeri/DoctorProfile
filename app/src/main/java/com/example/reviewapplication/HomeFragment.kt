fa6cc8d71e7f4d6fa2b181b3e3726ef1d5a5744e
Elahe Ameri <elahelastname@gmail.com>
2022-08-31 03:56:33 -0700

    add home Fragment

---
+++ app/src/main/java/com/example/reviewapplication/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.reviewapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.reviewapplication.databinding.FragmentHomeBinding
+import com.example.reviewapplication.databinding.FragmentLoginBinding
+
+
+class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/reviewapplication/LoginFragment.kt
+++ app/src/main/java/com/example/reviewapplication/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.example.reviewapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.reviewapplication.databinding.FragmentLoginBinding
 
 
@@ -36,7 +39,8 @@ class LoginFragment : Fragment() {
                 binding.etNumber.error="شماره تفن باید 11 رقمی باشد "
 
             }else{
-                binding.tvShowName.text = " سلام ${binding.etName.text}"
+                Toast.makeText(requireContext(),"سلام${binding.etName.text}",Toast.LENGTH_LONG).show()
+                findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
             }
         }
     }
